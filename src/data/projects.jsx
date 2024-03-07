2aed26ff7e2ef738e5ea96b8e3c719836713ddc3
HARRISON HASSAN <oluwakoredeadegbehingbe@gmail.com>
2024-03-07 20:23:15 +0100

    Configured projects object

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,5 +1,19 @@
 export const projects = [
-    {
-        
-    },
-]
+  {
+    id: "jfw92kc$",
+    name: "",
+    description: "",
+    overview: "",
+    coverPicture: "",
+    images: [],
+    tools: [],
+    domain: "",
+    projectType: "",
+    role: "",
+    githubLink: "",
+    postmanLink: "",
+    liveLink: "",
+    googleDriveLink: "",
+    figmaLink: "",
+  },
+];
