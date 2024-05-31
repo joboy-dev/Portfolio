a7cba66b5d46f08414b8e62df4bfa32be82710cc
Adegbehingbe, Oluwakorede Joseph <oluwakoredeadegbehingbe@gmail.com>
2024-05-31 20:58:04 +0100

    UI updates

--- src/data/skills.jsx
+++ src/data/skills.jsx
@@ -8,6 +8,7 @@ export const skills = [
     {name: 'HTML', proficiency: 100, image: 'assets/data_images/skills/html.svg'},
     {name: 'CSS', proficiency: 80, image: 'assets/data_images/skills/css.svg'},
     {name: 'JavaScript', proficiency: 85, image: 'assets/data_images/skills/js.svg'},
+    {name: 'FastAPI', proficiency: 90, image: 'assets/data_images/skills/fastapi.svg'},
     {name: 'React', proficiency: 80, image: 'assets/data_images/skills/react.svg'},
     {name: 'Tailwind CSS', proficiency: 85, image: 'assets/data_images/skills/tailwind.svg'},
     {name: 'Bootstrap CSS', proficiency: 80, image: 'assets/data_images/skills/bootstrap.svg'},
